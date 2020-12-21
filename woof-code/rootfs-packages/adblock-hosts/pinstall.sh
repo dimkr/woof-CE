c0cd6a103ebd83f2be9a538fa34fe343e8194661
Dima Krasner <dima@dimakrasner.com>
2020-12-21 10:09:52 +0200

    make pup-advert-blocker use github.com/StevenBlack/hosts, and run it headless in 3builddistro

--- woof-code/rootfs-packages/adblock-hosts/pinstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-wget --tries=1 --timeout=10 -O- https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | grep '^0\.0\.0\.0' | grep -v '^0\.0\.0\.0 [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$' >> etc/hosts
