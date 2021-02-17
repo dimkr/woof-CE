f374fc7e63c4e392e3068c89a67bc2cbef216344
Dima Krasner <dima@dimakrasner.com>
2021-02-17 23:03:01 +0200

    add a test that opens a terminal window

--- ci/test.sh
+++ ci/test.sh
@@ -1,24 +1,46 @@
+#!/bin/bash -ex
 
-#!/bin/sh -ex
+command_qemu() {
+    echo "$1" >> /tmp/qemu.in
+}
 
-(
-    sleep 360
-    echo screendump /tmp/quicksetup.pnm
+wait_for_screenshot() {
+    started=0
+    for i in `seq 1 $1`; do
+        echo $i 1>&2
+        command_qemu "screendump /tmp/$2.pnm"
+        sleep 1
+        composite -compose atop mask.xpm /tmp/$2.pnm /tmp/$2-masked.bmp
+        cmp /tmp/$2.bmp /tmp/$2-masked.bmp > /dev/null || continue
+        started=1
+        break
+    done
+    test $started -eq 1
+}
 
-    echo sendkey alt-f4
-    sleep 10
-    echo screendump /tmp/welcome1stboot.pnm
+[ -p /tmp/qemu.in ] || mkfifo /tmp/qemu.in
+[ -p /tmp/qemu.out ] || mkfifo /tmp/qemu.out
 
-    echo sendkey alt-f4
-    sleep 2
-    echo screendump /tmp/desktop.pnm
+if [ -n "$GITHUB_ACTIONS" ]; then
+    qemu-system-x86_64 -m 512 -drive format=raw,file=$1 -monitor pipe:/tmp/qemu -vga cirrus -display none &
+else
+    qemu-system-x86_64 -m 512 -drive format=raw,file=$1 -monitor pipe:/tmp/qemu -vga cirrus &
+fi
 
-    echo quit
-
-) | qemu-system-x86_64 -m 512 -drive format=raw,file=$1 -display none -monitor stdio -vga cirrus
-
-for SHOT in quicksetup welcome1stboot desktop; do
-    convert ${SHOT}.pnm /tmp/${SHOT}.bmp
-    composite -compose atop mask.xpm /tmp/${SHOT}.pnm /tmp/${SHOT}-masked.bmp
-    cmp /tmp/${SHOT}.bmp /tmp/${SHOT}-masked.bmp
+for SHOT in *.pnm; do
+    convert ${SHOT} /tmp/${SHOT%.pnm}.bmp
 done
+
+# wait until the desktop is ready
+wait_for_screenshot 360 quicksetup
+
+command_qemu "sendkey alt-f4"
+wait_for_screenshot 10 welcome1stboot
+
+command_qemu "sendkey alt-f4"
+wait_for_screenshot 5 desktop
+
+command_qemu "sendkey ctrl-alt-t"
+wait_for_screenshot 10 terminal
+
+command_qemu quit
