a6eba5f604fb5e9743e405ca0b99c6aa3ae4821f
Dima Krasner <dima@dimakrasner.com>
2022-04-26 01:24:03 +0800

    implement Alt-tab

--- woof-code/rootfs-petbuilds/dwl/config.h
+++ woof-code/rootfs-petbuilds/dwl/config.h
@@ -83,7 +83,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_h,          setmfact,       {.f = -0.05} },
 	{ MODKEY,                    XKB_KEY_l,          setmfact,       {.f = +0.05} },
 	{ MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
-	{ MODKEY,                    XKB_KEY_Tab,        view,           {0} },
+	{ MODKEY,                    XKB_KEY_Tab,        nextstacked,    {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,     {0} },
 	{ MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
