fe8518a3cedad1cd6bbb6de1dc4b31fe2bc5dd95
Dima Krasner <dima@dimakrasner.com>
2022-05-25 20:27:46 +0800

    cleanup

--- woof-code/rootfs-petbuilds/weechat/pinstall.sh
+++ woof-code/rootfs-petbuilds/weechat/pinstall.sh
@@ -1,3 +1,6 @@
+chroot . weechat -r "/server add libera irc.libera.chat/6697 -autoconnect -ssl;/set irc.server.libera.autojoin #puppylinux;/quit"
+chroot . run-as-spot weechat -r "/server add libera irc.libera.chat/6697 -autoconnect -ssl;/set irc.server.libera.autojoin #puppylinux;/quit"
+
 echo '#!/bin/ash
 exec weechat-shell' > usr/local/bin/defaultchat
 chmod 755 usr/local/bin/defaultchat
