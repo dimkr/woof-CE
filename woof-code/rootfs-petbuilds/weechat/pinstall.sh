7cbec99cadd702d5e4d271de90894e4a2697e84a
Dima Krasner <dima@dimakrasner.com>
2022-05-26 01:55:17 +0800

    fix breakage

--- woof-code/rootfs-petbuilds/weechat/pinstall.sh
+++ woof-code/rootfs-petbuilds/weechat/pinstall.sh
@@ -1,15 +1,3 @@
-if [ -f ../adrv/usr/bin/weechat-headless ]; then
-	mv -f ../adrv/usr/bin/weechat-headless usr/bin/
-	cp -r ../adrv/usr/lib/weechat usr/lib/
-	chroot . weechat-headless -r "/server add libera irc.libera.chat/6697 -autoconnect -ssl;/set irc.server.libera.autojoin #puppylinux;/quit"
-	chroot . run-as-spot weechat-headless -r "/server add libera irc.libera.chat/6697 -autoconnect -ssl;/set irc.server.libera.autojoin #puppylinux;/quit"
-	rm -rf usr/lib/weechat
-else
-	chroot . weechat-headless -r "/server add libera irc.libera.chat/6697 -autoconnect -ssl;/set irc.server.libera.autojoin #puppylinux;/quit"
-	chroot . run-as-spot weechat-headless -r "/server add libera irc.libera.chat/6697 -autoconnect -ssl;/set irc.server.libera.autojoin #puppylinux;/quit"
-fi
-rm -f usr/bin/weechat-headless
-
 echo '#!/bin/ash
 exec weechat-shell' > usr/local/bin/defaultchat
 chmod 755 usr/local/bin/defaultchat
