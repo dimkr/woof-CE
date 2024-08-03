70b0ca7f29821d6152c27d2a8ef2ea1539ba55a9
Dima Krasner <dima@dimakrasner.com>
2024-08-03 12:12:33 +0300

    Reapply "if cdrtools mkisofs is missing, fall back to xorriso"
    This reverts commit 977f5851b598336355a6535afd452857b869b247.

--- woof-code/support/mk_iso.sh
+++ woof-code/support/mk_iso.sh
@@ -344,12 +344,14 @@ mk_iso() {
 	tmp_isoroot=$1 	# input
 	OUTPUT=$2 		# output
 	BOOT_CAT="-c boot/boot.catalog"
+	MKISOFS="mkisofs"
+	command -v mkisofs > /dev/null || MKISOFS="xorriso -as mkisofs"
 	if [ "$UEFI_ISO" ] ; then
-		mkisofs -iso-level 4 -D -R -o $OUTPUT -b isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table ${BOOT_CAT} -isohybrid-mbr rootfs-complete/usr/lib/ISOLINUX/isohdpfx.bin \
+		${MKISOFS} -iso-level 4 -D -R -o $OUTPUT -b isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table ${BOOT_CAT} isohybrid-mbr rootfs-complete/usr/lib/ISOLINUX/isohdpfx.bin \
 			-eltorito-alt-boot -eltorito-platform efi -b boot/efi.img -no-emul-boot "$tmp_isoroot" || exit 100
 		[ $? -ne 0 ] && exit 1
 	else
-		mkisofs -iso-level 4 -D -R -o $OUTPUT -b isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table ${BOOT_CAT} "$tmp_isoroot" || exit 101
+		${MKISOFS} -iso-level 4 -D -R -o $OUTPUT -b isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table ${BOOT_CAT} "$tmp_isoroot" || exit 101
 		[ $? -ne 0 ] && exit 1
 	fi
 }
